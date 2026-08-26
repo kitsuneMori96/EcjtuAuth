48e905d5fb3b1625c3ed466c731fc924063bf38b
kitsuneMori <2794637787@qq.com>
2026-08-26 14:53:22 +0800

    fix: compileSdk 提升至 37 以满足 permission_handler 要求
    本机 release 构建验证通过 (app-release.apk 52.2MB)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "cn.kitsunemori.auin_ecjtu_wifi"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 37
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
